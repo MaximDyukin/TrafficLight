0c08879c95e46a102870807d5cd619cb6eb26e82
Max <maximdyukin@gmail.com>
2021-08-24 01:43:43 +0500

    Completely finished the user interface

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        startButton.layer.cornerRadius = 15
     }
     
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.height / 2
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
+    }
 }
 
